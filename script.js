f7080ea91835ce76a93abf4d9761987ff8e2f2dc
Ayush <aayushjn.0248@gmail.com>
2020-10-29 09:55:24 +0530

    Coding Challenge 3 completed

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const dice1 = document.querySelector(".dice--1");
+const dice2 = document.querySelector(".dice--2");
+
 var scores,roundScore,activePlayer,dice, playState,target;
 init();
 document.querySelector(".inp-final-score").addEventListener('keypress',function(inputKey){
@@ -10,33 +13,32 @@ document.querySelector(".inp-final-score").addEventListener('keypress',function(
 });
 var rollBtn = document.querySelector('.btn--roll');
 rollBtn.addEventListener('click', function () {
-    if(playState){
-        var dice = Math.ceil(Math.random()*6);
-        var diceDOM = document.querySelector('.dice');
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-'+dice+'.png';
-
-        if(dice !== 1){
-            if(dice === 6){
-                if(alreadyGotSix){
+    if(!target){
+        alert("Enter the final score and press Enter");
+    }
+    else{
+        if(playState){
+            var val1 = Math.ceil(Math.random()*6);
+            var val2 = Math.ceil(Math.random()*6);
+            dice1.style.display = 'block';
+            dice2.style.display = 'block';
+            dice1.src = 'dice-'+val1+'.png';
+            dice2.src = 'dice-'+val2+'.png';
+    
+            if(val1 !== 1 && val2 !== 1 ){
+                if(val1 === 6 && val2 === 6){
                     scores[activePlayer] = 0;
                     document.getElementById('score--'+activePlayer).textContent = scores[activePlayer];
                     nextPlayer();
                 }
                 else{
-                    alreadyGotSix = true;
-                    roundScore += dice;
+                    roundScore += val1 + val2;
                     document.getElementById('current--'+activePlayer).textContent = roundScore;
                 }
             }
             else{
-                alreadyGotSix = false;
-                roundScore += dice;
-                document.getElementById('current--'+activePlayer).textContent = roundScore;
+                nextPlayer();
             }
-        }
-        else{
-            nextPlayer();
         }
     }
 });
@@ -50,7 +52,8 @@ document.querySelector('.btn--hold').addEventListener('click', function() {
             target = NaN;
             playState = false;
             document.getElementById('name--'+activePlayer).textContent = 'Winner!!!';
-            document.querySelector('.dice').style.display = 'none';
+            dice1.style.display = 'none';
+            dice2.style.display = 'none';
             document.querySelector('.player--'+activePlayer).classList.add('player--winner');
             document.querySelector('.player--'+activePlayer).classList.remove('player--active');
         }
@@ -62,18 +65,19 @@ document.querySelector('.btn--hold').addEventListener('click', function() {
 
 function nextPlayer(){
     roundScore = 0;
-    alreadyGotSix = false;
     document.getElementById('current--'+activePlayer).textContent = 0;
     document.querySelector('.player--'+activePlayer).classList.remove('player--active');
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     document.querySelector('.player--'+activePlayer).classList.add('player--active');
 
-    document.querySelector('.dice').style.display = 'none';
+    dice1.style.display = 'none';
+    dice2.style.display = 'none';
 }
 
 document.querySelector('.btn--new').addEventListener('click', init);
 
 function init() {
+    document.querySelector(".inp-final-score").value = "";
     if(target)
         playState = true;
     else
@@ -81,14 +85,14 @@ function init() {
     scores = [0,0];
     roundScore = 0;
     activePlayer = 0;
-    alreadyGotSix = false;
 
     document.getElementById('current--0').textContent = '0';
     document.getElementById('current--1').textContent = '0';
     document.getElementById('score--0').textContent = '0';
     document.getElementById('score--1').textContent = '0';
 
-    document.querySelector('.dice').style.display = 'none';
+    dice1.style.display = 'none';
+    dice2.style.display = 'none';
 
     document.getElementById('name--'+0).textContent = 'Player 1';
     document.getElementById('name--'+1).textContent = 'Player 2';
